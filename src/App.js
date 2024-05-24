969dd792b2e3b3578ee6984a02a509e254870f8f
santosh <santosh33@gmail.com>
2024-05-24 20:01:50 +0200

    tailwind connected to application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
     <div>
-      Hello World
+      <h1 className='underline text-3xl'>Hello</h1>
     </div>
   )
 }
