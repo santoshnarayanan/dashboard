ad8f59a8692fa11307145ccae25b8593619c53fd
Santosh Narayanan <santosh33@gmail.com>
2024-05-30 20:19:44 +0200

    Toggle sidebar with NavButton

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
             {/* -----------------Display Tooltip------------------- */}
             <TooltipComponent content="Menu" position="BottomCenter">
               <button type="button" onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:display-none"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
                 >
                 <MdOutlineCancel />
               </button>
