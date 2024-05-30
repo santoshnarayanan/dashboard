e5d58df60cbf1cd7593748696647139ee3226279
Santosh Narayanan <santosh33@gmail.com>
2024-05-30 12:35:48 +0200

    Updated SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
 const Sidebar = () => {
   const activeMenu = true;
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -20,19 +22,36 @@ const Sidebar = () => {
               <SiShopware />
               <span>Shoppy</span>
             </Link>
+            {/*-----------------Display Tooltip------------------- */}
             <TooltipComponent content="Menu" position="BottomCenter">
               <button type="button" onClick={() => {}}
-                className="text-xl rounded-full p-3 hover:bg-light-gray 
-                mt-4 block md:hidden"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
                 >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
           </div>
+          {/*-----------------Display links on side bar and looping through links-------------------- */}
           <div className="mt-10">
             {links.map((item)=>(
-              <div className='text-gray-400 m-3 mt-4 uppercase'>
+              <div key={item.title}>
+                <p className='text-gray-400 m-3 mt-4 uppercase'>
                   {item.title}
+                </p>
+                {item.links.map((link)=>(
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => {}}
+                    className={({ isActive }) => isActive ? activeLink : normalLink}
+                  >
+                    {/* display sub-links */}
+                    {link.icon}
+                    <span className="capitalize">
+                      {link.name}
+                    </span>
+                  </NavLink>
+                ))}
               </div>
             ))}
           </div>
